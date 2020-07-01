a45a5bd835704b5a41e740cd4a1d09cab91c7f09
Trương Quang Tuan <truongquangtuan3110@gmail.com>
2020-07-01 16:40:55 +0700

    three

---
+++ template/contact/MS_CONTACT_VIECLAM_0007.php
@@ -0,0 +1,10 @@
+<style>
+    @media screen and (max-width: 991px) {
+        .text-wellcome {
+            text-align: left !important;
+        }
+    }
+</style>
+<div class="container" style="margin-top: 10px;">
+    <p class="text-wellcome" style="text-align: center;font-style: italic;font-size: 13px;"><?= $rowConfig['text_2'] ?></p>
+</div>
